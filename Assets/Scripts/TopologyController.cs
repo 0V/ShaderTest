63f3727df1360a83bb1060b2531ba03f725ccfa4
G2 <g2simm+0v@gmail.com>
2018-08-26 14:04:55 +0900

    Add topology controller

---
+++ Assets/Scripts/TopologyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopologyController : MonoBehaviour
+{
+    [SerializeField]
+    private MeshTopology topology;
+
+    void Start()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), topology, 0);
+    }
+}
